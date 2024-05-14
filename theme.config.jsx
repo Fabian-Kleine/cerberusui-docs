d40544561efc2db9fe84ea744f05b3d8c30ecbef
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-05-15 01:39:17 +0200

    feat: docsRepositoryBase

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,5 +2,6 @@ export default {
     logo: <span>Placholder Logo</span>,
     project: {
         link: 'https://github.com/Fabian-Kleine/cerberusui'
-    }
+    },
+    docsRepositoryBase: "https://github.com/Fabian-Kleine/cerberusui-docs/tree/main"
 }
