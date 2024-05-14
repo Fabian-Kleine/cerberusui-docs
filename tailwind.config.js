8379de3468309fbe661c5f470a844e8a634086a4
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-05-14 22:41:45 +0200

    feat: install cerberusui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        cuiprimary: "#3D78CD",
+        cuiprimaryhover: "#3160a4",
+        cuiprimarydisabled: "#244b82",
+        cuiprimarybg: "#0c1829",
+        cuisecondary: "#BD342F",
+        cuisecondaryhover: "#972a26",
+        cuisecondarydisabled: "#7a211d",
+        cuisecondarybg: "#4c1513",
+        cuitertiary: "#BDE763",
+        cuitertiaryhover: "#97b94f",
+        cuitertiarydisabled: "#6f8a37",
+        cuitertiarybg: "#39451e",
+        cuidefault: "#52706E",
+        cuidefaulthover: "#425a58",
+        cuidefaultdisabled: "#3c4f4e",
+        cuiwarning: "#ffd60a",
+        cuiwarninghover: "#ccab08",
+        cuiwarningdisabled: "#edc531",
+        cuiwarningbg: "#998006",
+        cuibase: "#020108",
+      }
+    }
   },
   plugins: [],
 };
