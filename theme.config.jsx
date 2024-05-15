5e2247a6b0151d0bb3a8de5b07c50858b7e2fcf5
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-05-15 12:27:42 +0200

    feat: update head data

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,5 +3,12 @@ export default {
     project: {
         link: 'https://github.com/Fabian-Kleine/cerberusui'
     },
-    docsRepositoryBase: "https://github.com/Fabian-Kleine/cerberusui-docs/tree/main"
+    docsRepositoryBase: "https://github.com/Fabian-Kleine/cerberusui-docs/tree/main",
+    head: (
+        <>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta property="og:title" content="Cerberusui" />
+          <meta property="og:description" content="A UI Library for when you have to create a doomsday app in Javascript" />
+        </>
+    )
 }
